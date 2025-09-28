d44f5fa03d1b1be74f03a2391b8c9b0640ed330b
Shariful Islam <sharifulislam246121@gmail.com>
2025-09-28 20:18:39 +0600

    update arrow function

---
+++ 2_Async_JS_inside_NodeJS/arrow.js
@@ -0,0 +1,19 @@
+
+let data = (param, param_two)=>{
+    console.log("Shariful islam");
+    console.log(param+param_two);
+}
+
+data(23,24);
+
+let new_arrow = () => console.log("hi....");
+new_arrow();
+
+let arr = ()=> 90;
+console.log(arr());
+
+// () = > 90
+
+let arr3 = ()=>{
+
+}
