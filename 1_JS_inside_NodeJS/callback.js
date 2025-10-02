9cb19fbd3130a049b92711fd8a18b1f70a637bdf
Shariful Islam <sharifulislam246121@gmail.com>
2025-10-02 16:22:23 +0600

    sync callback & arrow fun.

--- 1_JS_inside_NodeJS/callback.js
+++ 1_JS_inside_NodeJS/callback.js
@@ -1,20 +1,28 @@
-//---------------------Callback Function ----------------------------------
+//--------------------- Callback Function ----------------------------------
+// - A function passed as an argument to another functio . Called (executed) inside that function
+// - Useful for async tasks (like API calls, DB ops) or sync reuse
 
-//Sync use of callback function
+// Sync use of callback function
 function callSomeOne(data){
     data();
 };
 
-cb = function(){
+// function to pass as callback
+const cb = function(){
     console.log("Its a callback function");
 }
 
 callSomeOne(cb);
 
-//Arrow function
+//--------------------- Arrow Function (ES6+) -------------------------------------
+// Shorter syntax for writing functions
 
-prints = ()=>{
+
+const prints = ()=>{
     console.log("Arrow function");
 };
-
 prints();
+
+// One-liner arrow function
+const add = (a,b) => a+b;  // Implicit return
+console.log(add(5,6));
